40f150f1b0564a16edd7edfdc29aba3e9fc847c6
Wagner Araújo <wagneraraujo15@gmail.com>
2022-02-09 05:58:40 -0300

    updates

--- src/api/associado/controllers/associado.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- *  associado controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::associado.associado');
--- src/api/associado/routes/associado.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * associado router.
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::associado.associado');
