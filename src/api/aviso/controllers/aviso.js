9c2a111c1f72b475590559211b8661195a38bc0f
Wagner Araújo <wagneraraujo15@gmail.com>
2022-03-11 20:25:54 -0300

    delete avisos

--- src/api/aviso/controllers/aviso.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- *  aviso controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::aviso.aviso');
--- src/api/aviso/services/aviso.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * aviso service.
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::aviso.aviso');
