c06120f0f3505f4fc93e972decd919412e1caeaa
Wagner <wagneraraujo15@gmail.com>
2022-05-17 10:01:33 -0300

    update config databasae

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,26 +1,49 @@
 const { parse } = require("pg-connection-string");
 
-module.exports = ({ env }) => {
-  const { host, port, database, user, password } = parse(env("DATABASE_URL"));
+// module.exports = ({ env }) => {
+//   const { host, port, database, user, password } = parse(env("DATABASE_URL"));
 
-  return {
-    defaultConnection: "default",
-    connections: {
-      default: {
-        connector: "default",
-        settings: {
-          client: "postgres",
-          host,
-          port,
-          database,
-          username: user,
-          password,
+//   return {
+//     defaultConnection: "default",
+//     connections: {
+//       default: {
+//         connector: "default",
+//         settings: {
+//           client: "postgres",
+//           host,
+//           port,
+//           database,
+//           username: user,
+//           password,
+//           ssl: {
+//             rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false),
+//           },
+//         },
+//         debug: false,
+//       },
+//     },
+//   };
+// };
+
+module.exports = ({ env }) => {
+  if (env("NODE_ENV") === "production") {
+    return {
+      connection: {
+        client: "postgres",
+        connection: {
+          host: env("DATABASE_HOST", "127.0.0.1"),
+          port: env.int("DATABASE_PORT", 5432),
+          database: env("DATABASE_NAME", "strapi"),
+          user: env("DATABASE_USERNAME", "strapi"),
+          password: env("DATABASE_PASSWORD", "strapi"),
+
           ssl: {
             rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false),
+            ca: env(DATABASE_CA),
           },
         },
         debug: false,
       },
-    },
-  };
+    };
+  }
 };
