d0751830c72bc841675c7d7fcb513142e21ed4d8
Wagner <wagneraraujo15@gmail.com>
2022-06-10 11:43:39 -0300

    update databse config

--- config/database.js
+++ config/database.js
@@ -1,11 +1,21 @@
-const path = require('path');
+const { parse } = require("pg-connection-string");
 
 module.exports = ({ env }) => ({
   connection: {
-    client: 'sqlite',
+    client: "postgres",
     connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      host: env("DATABASE_HOST", "oregon-postgres.render.com"),
+      port: env.int("DATABASE_PORT", 5432),
+      database: env("DATABASE_NAME", "strapi_c2mz"),
+      user: env("DATABASE_USERNAME", "strapi_c2mz_user"),
+      password: env("DATABASE_PASSWORD", "hIE1G8GX8TnoRmomBxxloJx6OyRgYSjB"),
+      schema: env("DATABASE_SCHEMA", "public"), // Not required
+      ssl: {
+        ssl: env("DATABASE_SSL", true),
+        rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false), // For self-signed certificates
+        contentSecurityPolicy: false,
+      },
     },
-    useNullAsDefault: true,
+    debug: false,
   },
 });
--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,42 +1,42 @@
-const { parse } = require("pg-connection-string");
+// const { parse } = require("pg-connection-string");
 
-module.exports = ({ env }) => {
-  const { host, port, database, user, password } = parse(env("DATABASE_URL"));
+// module.exports = ({ env }) => {
+//   const { host, port, database, user, password } = parse(env("DATABASE_URL"));
 
-  return {
-    defaultConnection: "default",
-    connections: {
-      default: {
-        connector: "default",
-        settings: {
-          client: "postgres",
-          host,
-          port,
-          database,
-          username: user,
-          password,
-        },
-      },
-    },
-  };
-};
-
-// module.exports = ({ env }) => ({
-//   connection: {
-//     client: "postgres",
-//     connection: {
-//       host: env("DATABASE_HOST", "127.0.0.1"),
-//       port: env.int("DATABASE_PORT", 5432),
-//       database: env("DATABASE_NAME", "strapi"),
-//       user: env("DATABASE_USERNAME", "strapi"),
-//       password: env("DATABASE_PASSWORD", "strapi"),
-//       schema: env("DATABASE_SCHEMA", "public"), // Not required
-//       ssl: {
-//         ssl: env("DATABASE_SSL", true),
-//         rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false), // For self-signed certificates
-//         contentSecurityPolicy: false,
+//   return {
+//     defaultConnection: "default",
+//     connections: {
+//       default: {
+//         connector: "default",
+//         settings: {
+//           client: "postgres",
+//           host,
+//           port,
+//           database,
+//           username: user,
+//           password,
+//         },
 //       },
 //     },
-//     debug: false,
-//   },
-// });
+//   };
+// };
+
+module.exports = ({ env }) => ({
+  connection: {
+    client: "postgres",
+    connection: {
+      host: env("DATABASE_HOST", process.env.DATABASE_HOST),
+      port: env.int("DATABASE_PORT", process.env.DATABASE_PORT),
+      database: env("DATABASE_NAME", process.env.DATABASE_NAME),
+      user: env("DATABASE_USERNAME", process.env.DATABASE_USERNAME),
+      password: env("DATABASE_PASSWORD", process.env.DATABASE_PASSWORD),
+      schema: env("DATABASE_SCHEMA", "public"), // Not required
+      ssl: {
+        ssl: env("DATABASE_SSL", true),
+        rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false), // For self-signed certificates
+        contentSecurityPolicy: false,
+      },
+    },
+    debug: false,
+  },
+});
