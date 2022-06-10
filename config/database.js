2a8dc4600cb846f116bc711e90087c0a6103f5d2
Wagner <wagneraraujo15@gmail.com>
2022-06-10 18:28:41 -0300

    update temp config for data

--- config/database.js
+++ config/database.js
@@ -1,21 +1,15 @@
-const { parse } = require("pg-connection-string");
+const path = require("path");
 
 module.exports = ({ env }) => ({
   connection: {
-    client: "postgres",
+    client: "sqlite",
     connection: {
-      host: env("DATABASE_HOST", "oregon-postgres.render.com"),
-      port: env.int("DATABASE_PORT", 5432),
-      database: env("DATABASE_NAME", "strapi_c2mz"),
-      user: env("DATABASE_USERNAME", "strapi_c2mz_user"),
-      password: env("DATABASE_PASSWORD", "hIE1G8GX8TnoRmomBxxloJx6OyRgYSjB"),
-      schema: env("DATABASE_SCHEMA", "public"), // Not required
-      ssl: {
-        ssl: env("DATABASE_SSL", true),
-        rejectUnauthorized: env.bool("DATABASE_SSL_SELF", false), // For self-signed certificates
-        contentSecurityPolicy: false,
-      },
+      filename: path.join(
+        __dirname,
+        "..",
+        env("DATABASE_FILENAME", ".tmp/data.db")
+      ),
     },
-    debug: false,
+    useNullAsDefault: true,
   },
 });
