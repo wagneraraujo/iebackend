662adaff30b3e866a4a5e26015a2cd7a1a020ef5
Wagner <wagneraraujo15@gmail.com>
2022-07-06 22:00:47 -0400

    update ieback

--- config/plugins.js
+++ config/plugins.js
@@ -1,17 +1,25 @@
 module.exports = ({ env }) => ({
-    // ...
-    email: {
-      config: {
-        provider: 'sendgrid',
-        providerOptions: {
-          apiKey: env('SENDGRID_API_KEY'),
-        },
-        settings: {
-          defaultFrom: 'clubeiestrada@gmail.com',
-          defaultReplyTo: 'clubeiestrada@gmail.com',
-        },
-        
+  email: {
+    config: {
+      provider: "sendgrid",
+      providerOptions: {
+        apiKey: env("SENDGRID_API_KEY"),
+      },
+      settings: {
+        defaultFrom: "clubeiestrada@gmail.com",
+        defaultReplyTo: "clubeiestrada@gmail.com",
       },
     },
-    // ...
-  });
+  },
+  "import-export-entries": {
+    enabled: true,
+  },
+  menus: {
+    config: {
+      maxDepth: 3,
+    },
+  },
+  // "random-sort": {
+  //   enabled: true,
+  // },
+});
