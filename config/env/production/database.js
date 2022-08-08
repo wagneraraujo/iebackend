716b841b2cd3fc91f8dbf7f01fdada83c45973f5
Wagner Araujo <wagneraraujo15@gmail.com>
2022-08-08 14:14:15 -0300

    config database connectiontimeout

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -38,5 +38,17 @@ module.exports = ({ env }) => ({
       },
     },
     debug: false,
+		 acquireConnectionTimeout: 600000,
+    pool: {
+      min: 0,
+      max: 100,
+      acquireTimeoutMillis: 300000,
+      createTimeoutMillis: 300000,
+      destroyTimeoutMillis: 50000,
+      idleTimeoutMillis: 300000,
+      reapIntervalMillis: 10000,
+      createRetryIntervalMillis: 2000,
+      propagateCreateError: false,
+    },
   },
 });
